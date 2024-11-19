3d109afc7a56182c0af776ff5a9bbdb33c1829cf
zeng qing <50618591@qq.com>
2024-11-19 23:02:19 +0800

    electron使用remote

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const { app, BrowserWindow } = require('electron')
+
+const remote=require('@electron/remote/main')
+const path = require("path");
+remote.initialize()
+
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1800,
-        height: 800
+        height: 800,
+        webPreferences:{
+            nodeIntegration:true,
+            contextIsolation:false
+        },
     })
-
+    remote.enable(win.webContents)
     win.loadFile('index.html')
+
+
 }
 
 app.whenReady().then(() => {
--- work.js
+++ work.js
@@ -1,10 +1,34 @@
-document.getElementById('fileInput').addEventListener('change', handleFile, false);
+//
+// try{
+//     console.log('11111111111111111111111111')
+
+const remote = window.require('@electron/remote')
+remote.dialog.showOpenDialog({
+    title:"选择导入的txt文件",
+    properties:['openFile','multiSelections'],
+    filters:[
+        {name:'Markdown files',extensions:['txt']}
+    ]
+
+})
+//     remote.dialog.showOpenDialog({
+//         title:"选择导入的txt文件",
+//         properties:['openFile','multiSelections'],
+//         filters:[
+//             {name:'Markdown files',extensions:['txt']}
+//         ]
+//     })
+//     console.log('22222222222222222222222222222')
+// }catch (err){
+//     document.getElementById('fileInput').addEventListener('change', handleFile, false);
+// }
+
 
 function handleFile(event) {
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             const data = e.target.result;
             // 这里可以使用SheetJS库来解析Excel文件
             const workbook = XLSX.read(data, {type: 'binary'});
@@ -12,7 +36,7 @@ function handleFile(event) {
             const firstSheetName = workbook.SheetNames[0];
             const worksheet = workbook.Sheets[firstSheetName];
             const jsonData = XLSX.utils.sheet_to_json(worksheet, {header: 1});
-            const stepData= handleJson(jsonData)
+            const stepData = handleJson(jsonData)
             echarts(stepData)
             // console.log(jsonData);
         };
@@ -22,15 +46,15 @@ function handleFile(event) {
     }
 }
 
-function  handleJson(jsonObject){
-    let dateTim=[]
-    let steps=[]
+function handleJson(jsonObject) {
+    let dateTim = []
+    let steps = []
     for (var key in jsonObject) {
         if (jsonObject.hasOwnProperty(key)) { // 确保属性是对象自身的，而不是从原型链继承的
             // console.log(key + ": " + jsonObject[key]);
-            for (var key1 in jsonObject[key]){
-                if(key1==='0'){
-                    jsonObject[key][key1]=formatDate(jsonObject[key][key1],'-')
+            for (var key1 in jsonObject[key]) {
+                if (key1 === '0') {
+                    jsonObject[key][key1] = formatDate(jsonObject[key][key1], '-')
                     dateTim.push(jsonObject[key][key1])
                     steps.push(jsonObject[key][1])
                 }
@@ -38,14 +62,14 @@ function  handleJson(jsonObject){
             }
         }
     }
-    return [dateTim,steps]
+    return [dateTim, steps]
 }
 
 //numb 浮点数
 // format 间隔字符
 //eg: formatDate(3.13,"-")
 function formatDate(numb, format) {
-    if(typeof(numb)==='number'){
+    if (typeof (numb) === 'number') {
         const time = new Date((numb - 1) * 24 * 3600000 + 1);
         time.setYear(time.getFullYear() - 70);
         time.setHours(time.getHours() - 8);
@@ -54,19 +78,19 @@ function formatDate(numb, format) {
         const date = time.getDate() - 1 + '';
         const hours = time.getHours().toLocaleString();
         const minutes = time.getMinutes();
-        const seconds=time.getSeconds().toLocaleString();
+        const seconds = time.getSeconds().toLocaleString();
         if (format && format.length === 1) {
             return year + format + (month < 10 ? '0' + month : month) + format + (date < 10 ? '0' + date : date) //+ ' ' + (hours < 10 ? '0' + hours : hours) + ':' + (minutes < 10 ? '0' + minutes : minutes)+':'+(seconds < 10 ? '0' + seconds : seconds);
         }
-        return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date)+ (hours < 10 ? '0' + hours : hours)+ (minutes < 10 ? '0' + minutes : minutes)+ (seconds < 10 ? '0' + seconds : seconds);
-    }else{
+        return year + (month < 10 ? '0' + month : month) + (date < 10 ? '0' + date : date) + (hours < 10 ? '0' + hours : hours) + (minutes < 10 ? '0' + minutes : minutes) + (seconds < 10 ? '0' + seconds : seconds);
+    } else {
         return numb
     }
 }
 
 var myChart
 
-function echarts(stepData){
+function echarts(stepData) {
     require.config({
         paths: {
             echarts: './js'
@@ -83,45 +107,45 @@ function echarts(stepData){
             //--- 折柱 ---
             myChart = ec.init(document.getElementById('main'));
             myChart.setOption({
-                tooltip : {
+                tooltip: {
                     trigger: 'axis'
                 },
                 toolbox: {
-                    show : true,
-                    feature : {
-                        mark : {show: true},
-                        dataZoom: {show:true},
-                        dataView : {show: true, readOnly: true},
-                        magicType : {show: true, type: ['line', 'bar']},
-                        restore : {show: true},
-                        saveAsImage : {show: true},
+                    show: true,
+                    feature: {
+                        mark: {show: true},
+                        dataZoom: {show: true},
+                        dataView: {show: true, readOnly: true},
+                        magicType: {show: true, type: ['line', 'bar']},
+                        restore: {show: true},
+                        saveAsImage: {show: true},
                     }
                 },
-                dataZoom : {
-                    show : true,
+                dataZoom: {
+                    show: true,
                     realtime: true,
-                    start : 50,
-                    end : 100
+                    start: 50,
+                    end: 100
                 },
-                calculable : true,
-                xAxis : [
+                calculable: true,
+                xAxis: [
                     {
-                        type : 'category',
-                        name:'日期',
-                        data : stepData[0]
+                        type: 'category',
+                        name: '日期',
+                        data: stepData[0]
                     }
                 ],
-                yAxis : [
+                yAxis: [
                     {
-                        type : 'value',
-                        name:'步数',
-                        splitArea : {show : true}
+                        type: 'value',
+                        name: '步数',
+                        splitArea: {show: true}
                     }
                 ],
-                series : [
+                series: [
                     {
-                        type:'bar',
-                        data:stepData[1]
+                        type: 'bar',
+                        data: stepData[1]
                     }
                 ]
             });
@@ -130,7 +154,7 @@ function echarts(stepData){
     )
 }
 
-window.addEventListener('resize',function (){
+window.addEventListener('resize', function () {
     myChart.resize();
 })
 
