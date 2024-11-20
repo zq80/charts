95e0f94c727f8aae973771e53f93fea2a08bb8c1
zeng qing <50618591@qq.com>
2024-11-20 14:35:12 +0800

    实现了在electron上显示echarts，使用eharts-all。js就不会引起require.config冲突

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const {app, BrowserWindow} = require('electron')
 
-const remote=require('@electron/remote/main')
+const remote = require('@electron/remote/main')
 const path = require("path");
 remote.initialize()
 
@@ -11,14 +11,14 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 1800,
         height: 800,
-        webPreferences:{
-            nodeIntegration:true,
-            contextIsolation:false
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
         },
     })
+    // console.log('require.config888888888888888888888888888888888888', require)
     remote.enable(win.webContents)
-    win.loadFile('index.html')
-
+    win.loadFile('index3.html')
 
 }
 
